d1b97c61cac57884abfa07d95fcd094cacbf32c0
Serhii Reznychenko <sergeyrez.sr@gmail.com>
2023-05-15 00:26:27 +0300

    Task 3. Html Helpers / Html Tags (Tag Helpers)

---
+++ Northwind.Web/TagHelpers/NorthwindImageLinkHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Northwind.Web.TagHelpers
+{
+    public static class NorthwindImageLinkHelper
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper helper, int imageId, string text)
+        {
+            return new HtmlString(string.Format("<a href='images/{0}'>{1}</a>", imageId, text));
+
+        }
+    }
+}
---
+++ Northwind.Web/TagHelpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Northwind.Web.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindImageTagHelper : TagHelper
+    {
+        [HtmlAttributeName("northwind-id")]
+        public int CategoryId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"images/{CategoryId}");
+        }
+    }
+}
