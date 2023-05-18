24fed03f7332d3f5f28b0bff2c352ed4484e86c8
Serhii Reznychenko <sergeyrez.sr@gmail.com>
2023-05-18 23:22:21 +0300

    Task 2. ViewComponents / PartialViews

---
+++ Northwind.Web/Views/Components/BreadcrumbsViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Northwind.Web.Views.Components
+{
+    [ViewComponent]
+    public class BreadcrumbsViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            var path = HttpContext.Request.Path.Value.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+            switch(path.Length)
+            {
+                case 0:
+                    return string.Empty;
+                case 1:
+                    return "Home > Entity";
+                case 2:
+                case 3:
+                    return $"Home > Entity > {Map(path[1])}";
+                default:
+                    return $"Home{HttpContext.Request.Path.Value.Replace("/", " > ")}";
+            }
+        }
+
+        private string Map(string value) => value.ToLower() switch
+        {
+            "create" => "Create New",
+            "edit" => "Edit",
+            _ => value
+        };
+
+        
+    }
+}
