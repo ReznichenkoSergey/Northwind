d3f40fb4f8663baba7c8e11bce5a2fa4286c6752
Serhii Reznychenko <sergeyrez.sr@gmail.com>
2023-05-29 23:10:26 +0300

    Task 3. Edit Data 1

--- Northwind.Web/Controllers/CollectionController.cs
+++ Northwind.Web/Controllers/CollectionController.cs
@@ -5,13 +5,9 @@ using Northwind.Database;
 using Northwind.Database.Tables;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
-using System.Net.Http.Headers;
-using System.Net.Mime;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Northwind.Web.Controllers
 {
